6950c82d6792135b5964368df05a9cc27c471e2b
tauha farooqui <farooquitauha@gmail.com>
2025-11-19 21:57:30 +0500

    new changes in B2B

--- webshop/public/js/customer.js
+++ webshop/public/js/customer.js
@@ -7,7 +7,8 @@ frappe.ui.form.on('Customer', {
 function update_action(frm){
     if(!frm.doc.__islocal){
         frm.add_custom_button(__('Customer Item B2B Table'), ()=>{
-            console.log("button clicked")
+            let params = new URLSearchParams(`customer=${frm.doc.name}`);
+            window.location.href = `/app/customer-item-b2b?${params.toString()}`
         }, __("Actions"))
     }
 }
