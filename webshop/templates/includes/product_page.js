19c11944a23a94a8d99494698abd94e0b16ab71a
tauha farooqui <farooquitauha@gmail.com>
2026-01-02 21:25:20 +0500

    Debug 4

--- webshop/templates/includes/product_page.js
+++ webshop/templates/includes/product_page.js
@@ -13,6 +13,7 @@ frappe.ready(function() {
 		},
 		callback: function(r) {
 			if(r.message) {
+				frappe.throw(cstr(r.message))
 				if(r.message.cart_settings.enabled) {
 					let hide_add_to_cart = !r.message.product_info.price
 						|| (!r.message.product_info.in_stock && !r.message.cart_settings.allow_items_not_in_stock);
